6d91fd011df0f64280999d80438ea4d209d0cf92
Ziad Suliman <78859835+ZiadMakkawi@users.noreply.github.com>
2025-11-08 16:26:32 +0000

    Update update_stats.py

--- update_stats.py
+++ update_stats.py
@@ -111,7 +111,7 @@ def update_readme(stats):
     
     readme_content = f"""# Hi there! 👋
 
-I'm a passionate developer working on various projects. Here are my GitHub statistics:
+I'm a Software Enginner working on various projects and currently Upskilling myself. Here are my GitHub statistics:
 
 ## 📊 GitHub Statistics
 
@@ -135,10 +135,12 @@ I'm a passionate developer working on various projects. Here are my GitHub stati
 ### 🛠️ Technologies & Tools
 
 ![Python](https://img.shields.io/badge/-Python-3776AB?style=flat-square&logo=python&logoColor=white)
-![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black)
+![java](https://img.shields.io/badge/-Java-007396?style=flat-square&logo=openjdk&logoColor=white)
+![React](https://img.shields.io/badge/-React-61DAFB?style=flat-square&logo=react&logoColor=black)
+![Typescript](https://img.shields.io/badge/-TypeScript-3178C6?style=flat-square&logo=typescript&logoColor=white)
 ![Git](https://img.shields.io/badge/-Git-F05032?style=flat-square&logo=git&logoColor=white)
 ![GitHub](https://img.shields.io/badge/-GitHub-181717?style=flat-square&logo=github)
-![VS Code](https://img.shields.io/badge/-VS%20Code-007ACC?style=flat-square&logo=visual-studio-code)
+![Docker](https://img.shields.io/badge/-Docker-2496ED?style=flat-square&logo=docker&logoColor=white)
 
 </div>
 
